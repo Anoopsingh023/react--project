35396b085ff01ef60e63a3d192782f89b9ed0dc4
Anoopsingh023 <148649256+Anoopsingh023@users.noreply.github.com>
2024-09-19 20:16:14 +0530

    post Services

---
+++ Blog/src/appwrite/config.js
@@ -0,0 +1,137 @@
+import conf from "../conf/conf";
+import { Client,ID , Databases,Storage, Query } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createpost({title, slug, content, featuredimage, status, userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredimage,
+                    status,
+                    userId,
+                }
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredimage, status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredimage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+        }
+
+    }
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+            return false
+        }
+
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+            return false
+        }
+
+    }
+
+    // file uplode service
+
+    async uplodeFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error)
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+
+}
+
+const service = new Service()
+
+export default service
